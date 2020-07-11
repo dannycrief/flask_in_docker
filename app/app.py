ccbe399de8bd86652d09fa2aa308824b5a663d9f
Stepan Kozurak <step.kozbvb@gmail.com>
2020-07-12 00:18:21 +0200

    Data updated

--- app/app.py
+++ app/app.py
@@ -1,14 +1,29 @@
-import os
-from flask import Flask
+from flask import Flask, jsonify
+import redis
 
+r = redis.Redis(host='redis', port=6379)
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
+
+
+def get_fibonacci(number):
+    if (number == 0) or (number == 1):
+        return number
+    return get_fibonacci(number - 1) + get_fibonacci(number - 2)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def hello():
+    return 'Hello'
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+@app.route("/<number>", methods=['GET'])
+def get_fibonacci_api(number):
+    number = int(number)
+    stored_value = r.get(number)
+    if stored_value:
+        # logger.info("For %s stored value is used" % number)
+        return jsonify({number: stored_value.decode()}), 200
+    new_value = get_fibonacci(number)
+    # logger.info("For %s new value is calculated" % number)
+    r.set(number, new_value)
+    return jsonify({number: new_value}), 200
